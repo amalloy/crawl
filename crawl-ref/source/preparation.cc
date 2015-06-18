4866f5f34d313cf7782ca752b92645c162e99577
Alan Malloy <alan@malloys.org>
2015-06-18 10:40:40 -0700

    Remove some commented out junk

--- crawl-ref/source/preparation.cc
+++ crawl-ref/source/preparation.cc
@@ -6,11 +6,4 @@
 
 #include "preparation-data.h"
 
-// void init_preparation_index()
-// {
-//     for (unsigned int i = PREP_FIRST_PREPARATION; i <= PREP_LAST_PREPARATION; ++i)
-//     {
-//         unsigned int index = i - PREP_FIRST_PREPARATION;
-//         preparation_data_map[static_cast<preparation_type>(i)] = &preparation_data[index];
-//     }
-// }
+// TODO probably can put some code here
